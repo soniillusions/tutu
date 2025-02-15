af9debd0a3833c8a6f4e1d3692a8905e36dcdf71
Alexey Maryshenkov <alekseycrosso@gmail.com>
2025-02-15 15:59:08 +0300

    add callback to Route model, edit views for Route

--- app/models/route.rb
+++ app/models/route.rb
@@ -4,4 +4,12 @@ class Route < ApplicationRecord
   has_many :railway_stations_routes
   has_many :railway_stations, through: :railway_stations_routes
   has_many :trains, foreign_key: :current_route_id, dependent: :nullify
+
+  before_create :set_name
+
+  private
+
+  def set_name
+    self.name = "#{self.railway_stations.first.title} - #{railway_stations.last.title}"
+  end
 end
