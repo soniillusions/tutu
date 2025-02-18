fa005acdc9eebbc6a8e771532915b266c6d7b81e
Alexey Maryshenkov <alekseycrosso@gmail.com>
2025-02-18 23:11:32 +0300

    add auto-numbering for carriage

--- app/models/carriage.rb
+++ app/models/carriage.rb
@@ -1,10 +1,19 @@
 class Carriage < ApplicationRecord
   belongs_to :train
 
-  validates :number, presence: true
+  before_validation :set_number, on: :create
+
+  validates :number, presence: true, uniqueness: { scope: :train_id }
 
   scope :economy, -> { where(type: 'EconomyCarriage') }
   scope :coupe, -> { where(type: 'CoupeCarriage') }
   scope :sv, -> { where(type: 'SvCarriage') }
   scope :seat_only, -> { where(type: 'SeatOnlyCarriage') }
+
+  private
+
+  def set_number
+    last_number = train.carriages.pluck(:number).max || 0
+    self.number = last_number + 1
+  end
 end
